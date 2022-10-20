270ecaff5832c4952ebbec92699f6f8a385a5d69
Vamsi Chinta <vamsi.v.chinta.1@gmail.com>
2022-10-20 13:25:03 -0400

    Update Python_Reference_Sheet.py

--- Python_Reference_Sheet.py
+++ Python_Reference_Sheet.py
@@ -47,6 +47,15 @@ df.filter(like='Estimate')
 
 df = df.loc[:, ~df.columns.str.contains('Margin')] # filtering out column names that contain a certain string
 
+'''Column Header Editting'''
+df.columns = df.columns.str.upper()
+df.columns = df.columns.str.lower()
+df.columns = df.columns.str.title() #camel casing
+df.columns = df.columns.str.strip() #to remove all the leading or trailing spaces
+df.columns = df.columns.map(lambda x : x.replace("-", "_").replace(" ", "_")) #a simple lambda function to replace the space and hyphen with underscore
+dict = {'Dept.1':'Dept','Class.1':'Class'}
+df1.rename(columns=dict, inplace=True)
+df = df[['Year'] + [ col for col in df.columns if col != 'Year' ] ] # moving column to first position of a df
 
 ```editting cells by columns```
 df['B'].str.replace('[^\w\s]', '') # removing special characters but not spaces from cells 
@@ -93,15 +102,7 @@ df = pd.concat([df1, df2])
 df.to_excel('cat_kst_df.xlsx', sheet_name='Sheet_name_1')
 
 
-'''Column Header Editting'''
-df.columns = df.columns.str.upper()
-df.columns = df.columns.str.lower()
-df.columns = df.columns.str.title() #camel casing
-df.columns = df.columns.str.strip() #to remove all the leading or trailing spaces
-df.columns = df.columns.map(lambda x : x.replace("-", "_").replace(" ", "_")) #a simple lambda function to replace the space and hyphen with underscore
-dict = {'Dept.1':'Dept','Class.1':'Class'}
-df1.rename(columns=dict, inplace=True)
-df = df[['Year'] + [ col for col in df.columns if col != 'Year' ] ] # moving column to first position of a df
+
 
 
 '''Column Values Editting'''
