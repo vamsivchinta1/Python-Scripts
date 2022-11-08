99dd5e678a8c9667a64e948e8fbb5d9e70574c0d
Vamsi Chinta <vamsi.v.chinta.1@gmail.com>
2022-11-08 09:21:57 -0500

    Update Python_Reference_Sheet.py

--- Python_Reference_Sheet.py
+++ Python_Reference_Sheet.py
@@ -92,7 +92,7 @@ data["First Name"]= data["First Name"].str.lower()
 '''Working w/ Dates'''
 df2['Date'] = pd.to_datetime(df.Year, format='%Y') #convert year column to date
 df['datetime'] = pd.to_datetime(df['datetime']).dt.date
-
+df['year'] = pd.DatetimeIndex(df['birth_date']).year #extracting year from date col
 
 
 
